b1c357c21ca2fd36a63a96cd3d58f2721d5bbd60
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-26 18:00:39 +0300

    Update and rename 8.c to 8.cpp

---
+++ Теория/8.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	/*	a) while (E) { S; ... continue; ... S; next:; } - правильно 
+	
+		b) do { S; ... continue; ... S; } while ( E ); next: ; ... - неправильно 
+		
+		c) for ( E1; E2; E3) { S; ... continue; ... S; next: ; } - неправильно 
+		
+		d) while ( E) { S; ... for (E1;E2;E3) { S; ... continue; ... S; } ... S; next:; } - неправильно 
+		
+		e) while ( E) { S; ... for (E1;E2;E3) { S; ... continue; ... S; next: ; } ... S;} - правильно 
+	*/
+	printf("a,e- true\nb,c,d- false");
+}
+
