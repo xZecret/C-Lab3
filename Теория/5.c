63a82cb677055b823ec1684ad5688ac293e1dbd2
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-26 17:49:14 +0300

    Update 5.c

--- Теория/5.c
+++ Теория/5.c
@@ -1,15 +1,11 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
-/*Свечин Валентин
-  ВПИ22
-  Лаба - 3, задание - 5
-*/
 
-int main(int argc, char* argv[])
+int main()
 {
-	/*
-		a) 	for ( ; e2 ; ) S; и while ( e2 ) S; - эквивалентны
-		b) 	for ( ; ; ) S; и while (1) S; - эквивалентны
-	*/
+ /*
+	a)  for ( ; e2 ; ) S; и while ( e2 ) S; - эквивалентны
+	b)  for ( ; ; ) S; и while (1) S; - эквивалентны
+ */
 }
