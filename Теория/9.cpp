bdb37bb990bfd4da670563947255899f4944e45d
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-26 18:03:48 +0300

    Update and rename 9.c to 9.cpp

---
+++ Теория/9.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	/*	a)  while ( E ) { S; ... break; ... S; next: ; } -НЕ ВЕРНО
+	
+		b)  while ( E ) { S; ... break; ... S; } next: ; ... -ВЕРНО
+		
+		c)  do { S; ... break; ... S; } while ( E ); next: ; ... - ВЕРНО
+		
+		d)  for ( E1; E2; E3) { S; ... break; ... S; next: ; } - НЕ ВЕРНО
+		
+		e)  while ( E) { S; ... for ( E1; E2; E3) { S; ... break; ... S; } next: ; ... S; - ВЕРНО
+		
+		f)  while ( E ) { S; ... for ( E1; E2; E3) { S; ... break; ... S; } ... S; next: ; } - НЕ ВЕРНО
+	*/
+	printf("b,c,e- true\na,d,f- false");
+}
+
