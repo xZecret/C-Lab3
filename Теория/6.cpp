60b0b2849bda789fefdfd784ae069b19559b175e
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-26 17:57:57 +0300

    Update and rename 6.c to 6.cpp

--- Теория/6.cpp
+++ Теория/6.cpp
@@ -1,12 +1,8 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <locale.h>
-/*Свечин Валентин
-  ВПИ22
-  Лаба - 3, задание - 6
-*/
 
-int main(int argc, char* argv[])
+int main()
 {
 	/*
 	1) while ( E1 )
@@ -14,6 +10,7 @@ int main(int argc, char* argv[])
 		if ( E2 ) continue; 
 		S; 
 	}
+	
 	Улучшенное
 		while(E1)
 		{
@@ -24,6 +21,7 @@ int main(int argc, char* argv[])
 
 	2) do { if ( E1 ) continue; else S1; S2; }
 		while ( E2 );
+		
 	Улучшенное
 	do {
 		if ( !E1 )
