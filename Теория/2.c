fe7ee714c9c1d722987f048ebfd96ad5cef2c884
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-26 17:44:22 +0300

    Add files via upload

---
+++ Теория/2.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <math.h>
+/*Свечин Валентин
+  ВПИ22
+  Лаба - 3, задание - 2
+*/
+
+int main(int argc, char* argv[])
+{
+	/*
+	Число итерация для цикла for может быть заранее определено, но это не обязательно.
+	*/
+}
+
